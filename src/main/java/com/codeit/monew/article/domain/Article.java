57ef4f6e665e002f2da7271c607c077ac7ebc436
김규섭 <pens9053@gmail.com>
2025-10-21 16:06:41 +0900

    style: 주석 제거 및 코드 포매팅 적용

--- src/main/java/com/codeit/monew/article/domain/Article.java
+++ src/main/java/com/codeit/monew/article/domain/Article.java
@@ -22,19 +22,6 @@ import lombok.NoArgsConstructor;
 @EntityListeners(AuditingEntityListener.class)
 public class Article {
 
-	// id            uuid PRIMARY KEY,
-	// source        varchar(20)  NOT NULL,
-	// source_url    varchar(500) NOT NULL UNIQUE,
-	// title         varchar(500) NOT NULL,
-	// publish_date  timestamptz  NOT NULL,
-	// summary       varchar(500),
-	// comment_count bigint       NOT NULL DEFAULT 0,
-	// view_count    bigint       NOT NULL DEFAULT 0,
-	// collected_at  timestamptz  NOT NULL DEFAULT now(),
-	// created_at    timestamptz  NOT NULL DEFAULT now(),
-	// updated_at    timestamptz  NOT NULL DEFAULT now(),
-	// deleted_at    timestamptz
-
 	@Id
 	private UUID id;
 
--- src/main/java/com/codeit/monew/common/dto/CursorPageResponse.java
+++ src/main/java/com/codeit/monew/common/dto/CursorPageResponse.java
@@ -1,6 +1,6 @@
 package com.codeit.monew.common.dto;
 
-public record CursorPageResponse (
+public record CursorPageResponse(
 
 ) {
 }
