31f655410dd4a439c2ee421b2551f68edf9ef73a
rlawnsry <color9117@naver.com>
2025-10-22 09:23:02 +0900

    feat: Notification 엔티티 추가

--- src/main/java/com/codeit/monew/notification/domain/Notification.java
+++ src/main/java/com/codeit/monew/notification/domain/Notification.java
@@ -1,4 +1,55 @@
 package com.codeit.monew.notification.domain;
 
-public class Notification {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.management.NotificationListener;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.codeit.monew.common.base.BaseUpdatableDomain;
+import com.codeit.monew.user.domain.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "notifications")
+@Getter @Setter
+@EntityListeners(NotificationListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification extends BaseUpdatableDomain {
+	// user_id uuid NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+	// confirmed boolean NOT NULL DEFAULT false,
+	// content varchar(500) NOT NULL,
+	// resource_type varchar(20) NOT NULL,
+	// resource_id uuid NOT NULL,
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Column(nullable = false)
+	private boolean confirmed;
+
+	@Column(nullable = false, length = 500)
+	private String content;
+
+	@Column(name = "resource_type", nullable = false, length = 20)
+	private String resourceType;
+
+	@Column(name = "resource_id", nullable = false)
+	private UUID resourceId;
 }
