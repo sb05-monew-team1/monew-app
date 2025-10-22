5259a7587a5b6ad5ed1274079b494cb21bd11d35
김규섭 <pens9053@gmail.com>
2025-10-22 09:23:50 +0900

    feat: ArticleRepository 정의

--- src/main/java/com/codeit/monew/article/repository/ArticleRepository.java
+++ src/main/java/com/codeit/monew/article/repository/ArticleRepository.java
@@ -1,4 +1,10 @@
 package com.codeit.monew.article.repository;
 
-public interface ArticleRepository {
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codeit.monew.article.domain.Article;
+
+public interface ArticleRepository extends JpaRepository<Article, UUID> {
 }
