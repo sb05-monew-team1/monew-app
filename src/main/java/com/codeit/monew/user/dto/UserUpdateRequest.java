84c3e478baebf9b65ad2035e3e485b0b0d74037e
yunji1014 <7787yunji@gmail.com>
2025-10-22 16:01:17 +0900

    로그인 리퀘스트와 업데이트 dto 작성, 간단한 유효성 검사

--- src/main/java/com/codeit/monew/user/dto/UserUpdateRequest.java
+++ src/main/java/com/codeit/monew/user/dto/UserUpdateRequest.java
@@ -1,4 +1,15 @@
 package com.codeit.monew.user.dto;
 
-public record UserUpdateRequest() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import javax.inject.Singleton;
+
+//사용자 정보 수정 요청 DTO
+//api/users/{userId} (PUT/PATCH)
+public record UserUpdateRequest(
+    //공백이 아닌 문자열 체크
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Size(min = 2, max = 20, message = "닉네임을 입력해주세요. (최대 10자)")
+    String nickname
+) {
 }
