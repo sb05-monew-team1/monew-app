372635e51eb13d3e521565ec76c2277dbdfd11e1
김규섭 <pens9053@gmail.com>
2025-10-22 18:18:24 +0900

    feat: Article 도메인 클래스의 관계 정의 및 관련 도메인 작성, ArticleView 도메인 클래스 작성

--- src/main/java/com/codeit/monew/user/domain/User.java
+++ src/main/java/com/codeit/monew/user/domain/User.java
@@ -1,4 +1,11 @@
 package com.codeit.monew.user.domain;
 
-public class User {
+import com.codeit.monew.common.base.BaseUpdatableDomain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseUpdatableDomain {
 }
