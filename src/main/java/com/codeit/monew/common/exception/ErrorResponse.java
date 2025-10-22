f8fc63f7f74aafd539a3b8d2801258d800dea592
sooyeonz <5196ksy@naver.com>
2025-10-22 17:05:07 +0900

    feat: 문자열 유사도 커스텀 예외 추가

--- src/main/java/com/codeit/monew/common/exception/ErrorResponse.java
+++ src/main/java/com/codeit/monew/common/exception/ErrorResponse.java
@@ -1,4 +1,14 @@
 package com.codeit.monew.common.exception;
 
+import lombok.Getter;
+
+@Getter
 public class ErrorResponse {
+	private final String code;
+	private final String message;
+
+	public ErrorResponse(ErrorCode errorCode) {
+		this.code = errorCode.name();
+		this.message = errorCode.getMessage();
+	}
 }
