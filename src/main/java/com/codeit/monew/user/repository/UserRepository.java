180ba057f45fc70a53a440908c087a9ebb9baf26
yunji1014 <7787yunji@gmail.com>
2025-10-22 18:01:50 +0900

    User 레포에 기능들 정의

--- src/main/java/com/codeit/monew/user/repository/UserRepository.java
+++ src/main/java/com/codeit/monew/user/repository/UserRepository.java
@@ -1,4 +1,19 @@
 package com.codeit.monew.user.repository;
 
-public interface UserRepository {
+import com.codeit.monew.user.domain.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+//User 엔티티의 데이터 접근..
+public interface UserRepository extends JpaRepository<User, Long> {
+  //이메일 중복 검사 및 로그인 시 사용
+  Optional<User> findByEmail(String email);
+
+  //회원가입시 이메일 중복 검사
+  boolean existsByEmail(String email);
+
+  //닉네임 수정시 중복 검사 (프로토타입엔 없음)
+
+
 }
