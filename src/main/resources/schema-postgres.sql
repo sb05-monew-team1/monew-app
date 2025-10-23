6ea88eccd90f62b4bc54c45b483bbcc15d838cec
김규섭 <pens9053@gmail.com>
2025-10-23 09:48:05 +0900

    chore: DML 작성 및 자동 초기화 설정 추가, DDL 일부 수정

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -90,6 +90,7 @@ CREATE TABLE article_views
     id              uuid PRIMARY KEY,
     article_id      uuid        NOT NULL REFERENCES articles (id) ON DELETE CASCADE,
     user_id         uuid        NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    created_at      timestamptz NOT NULL DEFAULT now(),
     first_viewed_at timestamptz NOT NULL DEFAULT now(),
     last_viewed_at  timestamptz NOT NULL DEFAULT now()
 );
