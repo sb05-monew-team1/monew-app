3d3e9113dc70ba840ae4c928bf9d313f83baaf87
yunji1014 <7787yunji@gmail.com>
2025-10-23 18:01:06 +0900

    config 파일 구조 변경

--- src/main/java/com/codeit/monew/user/repository/UserRepository.java
+++ src/main/java/com/codeit/monew/user/repository/UserRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.CrudRepository;
 
 //User 엔티티의 데이터 접근..
 public interface UserRepository extends JpaRepository<User, UUID> {
-  //이메일 중복 검사 및 로그인 시 사용
+  //Spring Security 및 로그인 처리를 위해 이메일로 사용자 조회
   Optional<User> findByEmail(String email);
 
   //회원가입시 이메일 중복 검사
