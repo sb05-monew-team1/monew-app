c49e21f2f0470671aede126c4fb4b138777da8cf
yunji1014 <7787yunji@gmail.com>
2025-10-23 16:35:49 +0900

    SecurityConfig 비밀번호 암호화 클래스 추가, spring security 의존성 추가

---
+++ src/main/java/com/codeit/monew/user/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.codeit.monew.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    //BCrypt는 단순히 입력을 1회 해시시키는 것이 아니라 솔트를 부여하여
+    //여러번 해싱하므로 더 안전하게 암호를 관리할 수 있다.
+    return new BCryptPasswordEncoder();
+  }
+}
