49fcd8a654a20d53830bcb5a61867524930578fe
yunji1014 <7787yunji@gmail.com>
2025-10-23 10:38:14 +0900

    deletedAt 수정

--- src/main/java/com/codeit/monew/user/domain/User.java
+++ src/main/java/com/codeit/monew/user/domain/User.java
@@ -30,7 +30,7 @@ public class User extends BaseUpdatableDomain {
   private String password; //암호화 되어야 함.
 
   //논리 삭제시간
-  private Instant deleteAt;
+  private Instant deletedAt;
 
   //회원가입을 위한 User 객체 생성
   public static User register(String email, String nickname, String password) {
@@ -52,7 +52,6 @@ public class User extends BaseUpdatableDomain {
   //논리 삭제 처리
   //논리삭제시 1일 뒤 물리 삭제
   public void softDelete() {
-    this.deleteAt = Instant.now();
-
+    this.deletedAt = Instant.now();
   }
 }
