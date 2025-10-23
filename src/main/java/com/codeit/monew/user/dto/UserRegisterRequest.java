9f5cef2b4ae178d1038bd63a6d96a7b90fb4dbc3
yunji1014 <7787yunji@gmail.com>
2025-10-23 10:11:16 +0900

    UserRegisterRequest 문자 길이 수정

--- src/main/java/com/codeit/monew/user/dto/UserRegisterRequest.java
+++ src/main/java/com/codeit/monew/user/dto/UserRegisterRequest.java
@@ -9,20 +9,20 @@ import jakarta.validation.constraints.Size;
 //회원가입
 //api/users (POST)
 public record UserRegisterRequest(
-    //닉네임
-    @NotBlank(message = "닉네임을 입력해 주세요.")
-    @Size(min = 1, max = 10, message = "닉네임을 입력해 주세요 (최대 10자)")
-    String nickname,
-
     //아이디(이메일)
     @NotBlank(message = "이메일을 입력해 주세요.")
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     String email,
 
+    //닉네임
+    @NotBlank(message = "닉네임을 입력해 주세요.")
+    @Size(min = 1, max = 20, message = "닉네임을 입력해 주세요 (최대 10자)")
+    String nickname,
+
     //비밀번호
     @NotBlank(message = "비밀번호를 입력해 주세요.")
     @Pattern(
-        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,30}$",
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,20}$",
         message = "영문과 숫자, 특수문자를 포함해 6자 이상 입력해 주세요"
     )
     String password
