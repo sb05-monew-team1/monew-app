e29691466a38099894b82cb44efa4da2d08961ce
김규섭 <pens9053@gmail.com>
2025-10-23 17:21:54 +0900

    fix: CommentMapper의 toDto 메소드 일부 수정

--- src/main/java/com/codeit/monew/comment/mapper/CommentMapper.java
+++ src/main/java/com/codeit/monew/comment/mapper/CommentMapper.java
@@ -9,6 +9,8 @@ import com.codeit.monew.comment.dto.CommentDto;
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
 
+	@Mapping(target = "articleId", source = "comment.article.id")
+	@Mapping(target = "userId", source = "comment.user.id")
 	@Mapping(target = "userNickname", source = "userNickname")
 	@Mapping(target = "likedByMe", source = "likedByMe")
 	CommentDto toDto(Comment comment, String userNickname, boolean likedByMe);
