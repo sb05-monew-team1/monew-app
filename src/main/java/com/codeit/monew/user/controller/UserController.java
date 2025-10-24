a31ac661afdd8de034e2bf687b86870e957b60a8
yunji1014 <7787yunji@gmail.com>
2025-10-24 13:40:15 +0900

    Put매핑 -> Patch로 변경

--- src/main/java/com/codeit/monew/user/controller/UserController.java
+++ src/main/java/com/codeit/monew/user/controller/UserController.java
@@ -9,9 +9,9 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +33,7 @@ public class UserController {
   }
 
   //닉네임수정
-  @PutMapping("/{userId}")
+  @PatchMapping("/{userId}")
   public ResponseEntity<UserDto> updateUser(
       @PathVariable UUID userId,
       @Valid @RequestBody UserUpdateRequest userUpdateRequest
