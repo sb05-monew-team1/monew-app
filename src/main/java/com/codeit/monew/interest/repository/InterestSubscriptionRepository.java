b0c0f2bf96a8ab1e5c4a651823aa40e948d00406
sooyeonz <5196ksy@naver.com>
2025-10-27 16:01:27 +0900

    feat: 목록 조회를 위한 repository 추가

--- src/main/java/com/codeit/monew/interest/repository/InterestSubscriptionRepository.java
+++ src/main/java/com/codeit/monew/interest/repository/InterestSubscriptionRepository.java
@@ -1,11 +1,17 @@
 package com.codeit.monew.interest.repository;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.codeit.monew.interest.domain.InterestSubscription;
 
 public interface InterestSubscriptionRepository extends JpaRepository<InterestSubscription, UUID> {
-
+	@Query("SELECT s.interest.id FROM InterestSubscription s WHERE s.user.id = :userId AND s.interest.id IN :interestIds")
+	Set<UUID> findInterestIdsByUserIdAndInterestIdsIn(@Param("userId") UUID userId,
+		@Param("interestIds") List<UUID> interestIds);
 }
