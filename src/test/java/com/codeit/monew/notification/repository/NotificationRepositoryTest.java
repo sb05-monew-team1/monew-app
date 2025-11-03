89db559e2a467fc79713e009a9aa1091f906daf8
rlawnsry <color9117@naver.com>
2025-11-03 14:17:23 +0900

    test: 알림 목록 조회 테스트 삭제

--- src/test/java/com/codeit/monew/notification/repository/NotificationRepositoryTest.java
+++ src/test/java/com/codeit/monew/notification/repository/NotificationRepositoryTest.java
@@ -2,7 +2,6 @@ package com.codeit.monew.notification.repository;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.time.Instant;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -45,40 +44,4 @@ public class NotificationRepositoryTest {
 		assertThat(second.resourceId()).isEqualTo(UUID.fromString("40000000-0000-0000-0000-000000000002"));
 		assertThat(second.resourceType()).isEqualTo("comments");
 	}
-
-	// @Test
-	// void searchNotificationsNextPage() {
-	// 	String userId = "11111111-1111-1111-1111-111111111111";
-	// 	Slice<NotificationDto> firstSlice = notificationRepository.search(null, null, 2, userId);
-	//
-	// 	assertThat(firstSlice.hasNext()).isTrue();
-	// 	assertThat(firstSlice.getContent()).hasSize(2);
-	// 	assertThat(firstSlice.getContent())
-	// 		.extracting(NotificationDto::id)
-	// 		.containsExactly(
-	// 			UUID.fromString("50000000-0000-0000-0000-000000000004"),
-	// 			UUID.fromString("50000000-0000-0000-0000-000000000003")
-	// 		);
-	// 	assertThat(firstSlice.getContent())
-	// 		.extracting(NotificationDto::content)
-	// 		.containsExactly("알림 테스트용 데이터3", "알림 테스트용 데이터2");
-	//
-	// 	NotificationDto lastNotification = firstSlice.getContent().get(1);
-	// 	String cursor = String.valueOf(lastNotification.createdAt());
-	// 	Instant after = lastNotification.createdAt();
-	//
-	// 	System.out.println("메서드 호출 전 cursor : " + cursor + ", after : " + after + ", userId: " + userId);
-	// 	Slice<NotificationDto> secondSlice = notificationRepository.search(cursor, after, 2, userId);
-	//
-	// 	assertThat(secondSlice.hasNext()).isTrue();
-	// 	assertThat(secondSlice.getContent())
-	// 		.extracting(NotificationDto::id)
-	// 		.containsExactly(
-	// 			UUID.fromString("50000000-0000-0000-0000-000000000005"),
-	// 			UUID.fromString("50000000-0000-0000-0000-000000000001")
-	// 		);
-	// 	assertThat(secondSlice.getContent())
-	// 		.extracting(NotificationDto::content)
-	// 		.containsExactly("알림 테스트용 데이터4", "알림 테스트용 데이터1");
-	// }
 }
